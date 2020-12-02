0494c82ed8f806a0f0973edf238715dcb8d0749e
WesleyVitor <wesleydemorais@outlook.com.br>
2020-12-01 23:05:18 -0300

    add: CRUD cliente

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-import emprestimos_metodos  as EM
-import clientes_metodos as CM
-import veiculos_metodos as VM 
 import view.menu
 import config.operacoes_SO
-print(view.menu.inicial())
-opcaoAdmin = input('ESCOLHA UMA OPÇÃO:')
-if opcaoAdmin == '1':
-  config.operacoes_SO.usando_clear()
-  print(view.menu.view_clientes())
-elif opcaoAdmin == '2':
-  config.operacoes_SO.usando_clear()
-  print(view.menu.view_veiculos())
-elif opcaoAdmin == '3':
-  config.operacoes_SO.usando_clear()
-  print(view.menu.view_emprestimos())
-else:
-  print("RESPEITE O MENU!!")
+import cliente_service 
+while True:
+  print(view.menu.inicial())
+  opcaoAdmin = input('ESCOLHA UMA OPÇÃO:')
+  if opcaoAdmin == '1':
+    sairCliente = 'N'
+    while sairCliente.upper()=='N':
+      config.operacoes_SO.usando_clear()
+      sairCadastro = 'N'
+      print(view.menu.view_clientes())
+      opcao = input("===DIGITE SUA OPÇÃO===")
+      cliente_service.geral(opcao)
+      print()
+      sairCliente = input("===QUE SAIR DA SESSÃO CLIENTE ?(S - N)")
+
+
+  elif opcaoAdmin == '2':
+    config.operacoes_SO.usando_clear()
+    print(view.menu.view_veiculos())
+  elif opcaoAdmin == '3':
+    config.operacoes_SO.usando_clear()
+    print(view.menu.view_emprestimos())
+  else:
+    print("RESPEITE O MENU!!")
 
 
 # CS.cadastrarClientes('122','Jose')
